0fbfa076049a309a72955fc0e9859f8bcfae275a
damda0904 <suen0904@gmail.com>
2021-12-09 20:13:14 +0900

    assessmentItem grouping

--- grouping.py
+++ grouping.py
@@ -42,7 +42,7 @@ for idx, row in errata.iterrows():
 
     if (row.answerCode == 1):
         profile = row.learnerProfile
-        data = {"testID": row.testID, "assessmentItemID": row.assessmentItemID}
+        data = {"learnerID": row.learnerID, "testID": row.testID, "assessmentItemID": row.assessmentItemID}
 
         if (profile == "F;S01;7" or profile == "M;S01;7"):
             addRow(idx_1, 1, data)
@@ -61,18 +61,134 @@ for idx, row in errata.iterrows():
     elif (idx % 5000 == 0):
         print(".", end=" ")
 
-print("====================================")
-print(len(errata_m1))
-print(len(errata_m2))
-print(len(errata_m3))
-
 errata_m1 = json.dumps(m1)
 errata_m2 = json.dumps(m2)
 errata_m3 = json.dumps(m3)
 
-with open('errata_m1.txt', 'w') as outfile:
+print("====================================")
+print(len(m1))
+print(len(m2))
+print(len(m3))
+
+with open('each_errata_m1.txt', 'w') as outfile:
     outfile.write(errata_m1)
-with open('errata_m2.txt', 'w') as outfile:
+with open('each_errata_m2.txt', 'w') as outfile:
     outfile.write(errata_m2)
-with open('errata_m3.txt', 'w') as outfile:
+with open('each_errata_m3.txt', 'w') as outfile:
     outfile.write(errata_m3)
+
+from ast import literal_eval
+
+# 필터링된 문항 데이터셋과 비교해서
+# 해당되는 맞은 문항을 학생별로 묶어서 저장
+
+# 학생별로 맞은 문항을 묶을 딕셔너리
+# 딕셔너리는 학년별로 구분
+s1 = {}
+s2 = {}
+s3 = {}
+
+# 문항 데이터셋을 위한 딕셔너리
+q1 = {}
+q2 = {}
+q3 = {}
+
+# Json 파일 읽기
+question1 = literal_eval(open('./m1_filter_guessLevel.txt', 'r').readline())
+question2 = literal_eval(open('./m2_filter_guessLevel.txt', 'r').readline())
+question3 = literal_eval(open('./m3_filter_guessLevel.txt', 'r').readline())
+
+# 각 문항 데이터셋을 딕셔너리로 전처리
+for row in question1:
+    q1[row['assessmentItemID']] = json.dumps(row)
+for row in question2:
+    q2[row['assessmentItemID']] = json.dumps(row)
+for row in question3:
+    q3[row['assessmentItemID']] = json.dumps(row)
+
+print(len(q1))
+print(len(q2))
+print(len(q3))
+print("====================================")
+print("start grouping")
+
+# 필터링 및 그룹핑
+for key, value in m1.items():
+    learner = value['learnerID']
+    assessmentItem = value['assessmentItemID']
+
+    # 문항 데이터셋에 들어있는 문항이 아닐 경우
+    if (not (assessmentItem in q1)):
+        continue
+
+    if (learner in s1):
+        tmp = s1[learner]
+        tmp.add(assessmentItem)
+        s1[learner] = tmp
+    else:
+        tmp = set()
+        tmp.add(assessmentItem)
+        s1[learner] = tmp
+
+for key, value in m2.items():
+    learner = value['learnerID']
+    assessmentItem = value['assessmentItemID']
+
+    # 문항 데이터셋에 들어있는 문항이 아닐 경우
+    if (not (assessmentItem in q2)):
+        continue
+
+    if (learner in s2):
+        tmp = s2[learner]
+        tmp.add(assessmentItem)
+        s2[learner] = tmp
+    else:
+        tmp = set()
+        tmp.add(assessmentItem)
+        s2[learner] = tmp
+
+for key, value in m3.items():
+    learner = value['learnerID']
+    assessmentItem = value['assessmentItemID']
+
+    # 문항 데이터셋에 들어있는 문항이 아닐 경우
+    if (not (assessmentItem in q3)):
+        continue
+
+    if (learner in s3):
+        tmp = s3[learner]
+        tmp.add(assessmentItem)
+        s1[learner] = tmp
+    else:
+        tmp = set()
+        tmp.add(assessmentItem)
+        s3[learner] = tmp
+
+print("")
+print("====================================")
+print(len(s1))
+print(len(s2))
+print(len(s3))
+
+for key, value in s1.items():
+    tmp = list(value)
+    s1[key] = tmp
+
+for key, value in s2.items():
+    tmp = list(value)
+    s2[key] = tmp
+
+for key, value in s3.items():
+    tmp = list(value)
+    s3[key] = tmp
+
+s1_assessmentItemSet = json.dumps(s1)
+s2_assessmentItemSet = json.dumps(s2)
+s3_assessmentItemSet = json.dumps(s3)
+
+with open('studentGroup_m1.txt', 'w') as outfile:
+    outfile.write(s1_assessmentItemSet)
+with open('studentGroup_m2.txt', 'w') as outfile:
+    outfile.write(s2_assessmentItemSet)
+with open('studentGroup_m3.txt', 'w') as outfile:
+    outfile.write(s3_assessmentItemSet)
